400d4f59235e171d2de275c167e0644e14b198fc
its-chrissy <christine.hamid@hotmail.com>
2023-06-26 18:55:17 -0400

    fixed nav and footer, created and styles comments, styled text boxes, added shows page

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const postComment = () => {
+  console.log("hello christine");
+
+  let postComment = document.querySelector(".data-comment-text").value;
+  console.log(postComment);
+  // take input
+  // create Element
+  /**
+ 
+<div class="comments__item">
+            <div class="comments__item--img"> <img src="." /> 
+            </div>
+						<div class="comments__name">Miles Acosta</div>
+						<div class="comments__content">
+							<p>
+								your text
+							</p>
+						</div>
+					</div>
+**/
+
+  // append to the list of comments
+};
+// createComment();
